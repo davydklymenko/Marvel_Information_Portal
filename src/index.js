de72d5f1fd488b01ddd38faa9c95e54b24e243c2
Davyd Klymenko <142334488+davydklymenko@users.noreply.github.com>
2024-02-19 12:22:15 +0200

    Random Characters

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './index.css';
-import MarvelService from './services/MarvelService';
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => console.log(res));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,11 @@
 
 class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=17c984d852299811a210c9b94f395735';
+
    getResource = async (url) => {
-        const res = await fetch(url);
+        let res = await fetch(url);
     
         if(!res.ok) {
             throw new Error(`Could not url ${url} status: ${res.status}`)
@@ -10,9 +14,28 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=17c984d852299811a210c9b94f395735');
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=220&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 230)}...Learn More on Wiki` : 'No description on this character.',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+
+
 }
 
 export default MarvelService;
